72535bc7d04f640dfd397fdbca8bcf1c5d8aff04
torrent benoit <torrentdu64@gmail.com>
2016-02-15 16:20:51 +0100

    commit a vide

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'games/game'
+
+  get 'games/score'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
